c2fe58350669a4cffcd56de0e1062290ae166219
Sergio Ortiz <saortizi@ufpso.edu.co>
2024-12-11 16:43:39 -0500

    Eliminada la columna state de la tabla information

---
+++ prisma/migrations/20241211160929_drop_colum_state_table_information/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `state` on the `information` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "information" DROP COLUMN "state";
